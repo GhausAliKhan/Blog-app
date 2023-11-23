baa6fce9660e1150cdc8ef7c83c46fd0be2ee626
GhausAliKhan <ghauskhan1616@gmail.com>
2023-11-23 14:48:44 +0500

    Setup association and methods to class Like by following ERD

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,13 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  # Callback to update the likes counter for a post
+  after_create :increment_post_likes_counter
+
+  private
+
+  def increment_post_likes_counter
+    post.increment!(:likes_counter)
+  end
 end
