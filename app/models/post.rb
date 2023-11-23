bd75875858acea483ba87181e3073a6a69362e0d
GhausAliKhan <ghauskhan1616@gmail.com>
2023-11-23 14:48:11 +0500

    Setup association and methods to class Post by following ERD

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,25 @@
 class Post < ApplicationRecord
-  belongs_to :author
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+
+  # Attributes
+  attribute :title, :string
+  attribute :text, :text
+  attribute :comments_counter, :integer, default: 0
+  attribute :likes_counter, :integer, default: 0
+
+  # Callback to update the posts counter for a user
+  after_create :increment_user_posts_counter
+
+  # A method which returns the 5 most recent comments for a given post
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  private
+
+  def increment_user_posts_counter
+    author.increment!(:posts_counter)
+  end
 end
