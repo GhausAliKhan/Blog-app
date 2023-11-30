0f82040a46829c95fa0436c393cf0c37ef8d2621
GhausAliKhan <ghauskhan1616@gmail.com>
2023-11-30 14:15:48 +0500

    Update posts_controller and add methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,64 @@
 class PostsController < ApplicationController
-  before_action :find_user, only: %i[index show]
-  before_action :find_post, only: [:show]
+  before_action :find_user, only: %i[show new create like]
+  before_action :find_post, only: %i[show like]
 
+  # GET /posts or /users/:user_id/posts
   def index
-    @posts = @user.posts
+    if params[:user_id]
+      @user = User.find_by(id: params[:user_id])
+      if @user
+        @posts = @user.posts
+      else
+        flash[:alert] = 'User not found.'
+        redirect_to root_path and return
+      end
+    else
+      @posts = Post.all
+    end
   end
 
+  # GET /users/:user_id/posts/:id
   def show; end
+
+  # GET /users/:user_id/posts/new
+  def new
+    @post = @user.posts.build
+  end
+
+  # POST /users/:user_id/posts
+  def create
+    @post = @user.posts.build(post_params)
+    if @post.save
+      redirect_to user_post_path(@user, @post), notice: 'Post was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # POST /users/:user_id/posts/:id/like
+  def like
+    @post.increment!(:likes_counter)
+    redirect_to [@user, @post], notice: 'You liked a post!'
+  end
 
   private
 
   def find_user
     @user = User.find(params[:user_id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:alert] = 'User not found.'
+    redirect_to root_path
   end
 
   def find_post
-    @post = @user.posts.find(params[:id])
+    @post = Post.find(params[:id])
   rescue ActiveRecord::RecordNotFound
-    flash[:alert] = 'Post not found. Redirecting to user posts.'
+    flash[:alert] = 'Post not found.'
     redirect_to user_posts_path(@user)
+  end
+
+  # Only allow a list of trusted parameters through.
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
