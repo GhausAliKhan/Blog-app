b7cd96f6582aac460a7c50e8683bccfa20dda3ef
GhausAliKhan <ghauskhan1616@gmail.com>
2023-11-30 14:06:44 +0500

    Create method current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  # Makes the current_user method available to views
+  helper_method :current_user
+
+  private
+
+  # Defines the method to find or create the current_user from the first user in the database:
+  def current_user
+    @current_user ||= User.first
+  end
 end
