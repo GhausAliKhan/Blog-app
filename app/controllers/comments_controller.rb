4a509fe314fdc99c6200ea7904290c853977aa76
GhausAliKhan <ghauskhan1616@gmail.com>
2023-11-30 14:19:27 +0500

    Implement comments_controller with new and create actions

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,20 @@
+class CommentsController < ApplicationController
+  before_action :find_user
+  before_action :find_post, only: %i[new create]
+
+  def new
+    @comment = @post.comments.new
+  end
+
+  def create
+    @comment = @post.comments.new(comment_params)
+    @comment.user = current_user # Assign the current user to the comment
+
+    if @comment.save
+      flash[:notice] = 'Comment created successfully.'
+      redirect_to user_post_path(@user, @post)
+    else
+      render :new
+    end
+  end
+end
