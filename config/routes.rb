65061c23d84d267c56b3691ffebcc7a60db9e864
GhausAliKhan <ghauskhan1616@gmail.com>
2023-12-06 13:33:42 +0500

    Configure Devise settings and integration

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  root "users#index"
+  # Change to point to 'home#index'
+  root "home#index"
   
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
